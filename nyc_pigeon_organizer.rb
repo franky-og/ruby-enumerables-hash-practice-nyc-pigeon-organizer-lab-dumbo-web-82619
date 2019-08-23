5b068d109b53dc62a3dddf0b5e90abb7706530f7
Franky Liu <cuifranky95@gmail.com>
2019-08-23 02:24:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-  names = []
-  name << data[:gender][:male]
+  # write your code here
+  new_data = {}
+  names = data[:gender][:male] + data[:gender][:female]
+  
+  names.map{|pigeon|
+    data[:color].map{|key_color, value_pigeons|
+      if value_pigeons
+    }
+    new_data[pigeon] = {
+      :color => ,
+      :gender => ,
+      :lives =>
+    }
+    
+  }
 end
